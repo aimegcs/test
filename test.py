b79a9378b9ae3716cb74d70b97115e18d4228a52
gago14 <naime@gcs.us.com>
2022-08-20 17:49:46 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,34 @@
+nombre = (1, 2, 3, 4, 5, 6)
+print("hash: ",hash(nombre))
+
+nombre2 = (1, 5, 3, 4, 5, 6)
+print("hash: ",hash(nombre2))
+
+note1 = [10, 20, 16, 12, 8]
+
+
+# print(id(note1))
+# print("note1 :", note1)
+# note1.append(98)
+# print(id(note1))
+# print("note1 :", note1)
+
+# print("note1 : ", note1)
+# note1.append(5)
+# print("note2 : ", note1)
+
+# print("nombre + nombre2 : ", nombre + nombre2)
+
+# personne = {
+#     "nom": "aime",
+#     "age": "20",
+#     "quartier": "bonoumin"
+# }
+# print('nom :', personne['nom'])
+# print(id(personne))
+
+
+# personne["nom"] = "Joseph"
+# print('nouveau nom: ', personne["nom"])
+# print(id(personne))
+
